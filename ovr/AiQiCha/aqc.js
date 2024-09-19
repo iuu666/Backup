538e48b3699fe30a4b393b325583bd9537068854
iuu <okgo.orz@gmail.com>
2024-09-20 03:22:42 +0800

    1

--- ovr/AiQiCha/aqc.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/\"svip\":\{\"status\":0,\"startTime\":\"\",\"endTime\":\"\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"2099-12-31\"");
-$done({ body });
