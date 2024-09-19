34f83c94a326eb10858276e5ae77a3a4383188bd
iuu666 <okgo.orz@gmail.com>
2024-09-20 03:07:28 +0800

    Update bm7.py

--- scripts/bm7.py
+++ scripts/bm7.py
@@ -1,4 +1,23 @@
 import requests
+from lxml import etree
+import json
+from datetime import datetime, timedelta
+
+# 生成头部信息
+def get_header(file_name, count):
+    utc_time = datetime.utcnow()  # 获取当前的 UTC 时间
+    china_time = utc_time + timedelta(hours=8)  # 转换为 CST (UTC+8)
+    local_time = china_time.strftime("%Y-%m-%d %H:%M:%S")  # 格式化时间为字符串
+    # 根据文件名生成相应的头部信息
+    header = f"""\
+// {file_name.replace('.list', '')}
+// Link: https://github.com/iuu666/ASN.China
+// Total Count: {count}
+// Last Updated: CST {local_time}
+// Made by iuu, All rights reserved.
+
+"""
+    return header
 
 def fetch_and_save(url, file_name):
     # 发起 GET 请求获取页面内容
