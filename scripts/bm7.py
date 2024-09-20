60531e3bbb97cea7dc5fb07db004fb2ee23794a5
iuu666 <okgo.orz@gmail.com>
2024-09-20 17:35:34 +0800

    Update bm7.py

--- scripts/bm7.py
+++ scripts/bm7.py
@@ -1,15 +1,14 @@
 import requests
 from lxml import etree
 import json
-from datetime import datetime
 
 # 新的 IP 列表的 URL
 Direct = "https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Surge/Direct/Direct.list"
 Hijacking = "https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Surge/Hijacking/Hijacking.list"
 Privacy = "https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Surge/Privacy/Privacy_All.list"
 
-def get_header(name, author, repo_url, total):
-    return f"// NAME: {name}\n// AUTHOR: {author}\n// REPO: {repo_url}\n// TOTAL: {total}\n\n"
+def get_header(name, repo_url, total):
+    return f"// NAME: {name}\n// AUTHOR: \n// REPO: {repo_url}\n// TOTAL: {total}\n\n"
 
 def fetch_and_save(url, file_name):
     r = requests.get(url)
@@ -17,14 +16,13 @@ def fetch_and_save(url, file_name):
     asns = tree.xpath('//*[@data-target="react-app.embeddedData"]')[0].text
     x = json.loads(asns)['payload']['blob']['rawLines']
     
-    repo_url = url.replace("blob/", "")  # 修改 REPO 地址为下载内容的地址
+    repo_url = url  # 保持原始链接
     total_count = len(x)  # 计算总行数
     name = file_name.split('.')[0]  # 提取文件名作为 NAME
-    author = "YourName"  # 替换为你的作者名
 
     with open(file_name, "w", encoding='utf-8') as file:
         # 写入头部信息
-        file.write(get_header(name, author, repo_url, total_count))
+        file.write(get_header(name, repo_url, total_count))
         for i in x:
             file.write(i)
             file.write('\n')
