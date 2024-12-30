9eeab9ca6cb07cf678c677aa316934d151a2b561
iuu666 <okgo.orz@gmail.com>
2024-12-31 00:51:32 +0800

    Create ZMFY.js

---
+++ js/ZMFY/ZMFY.js
@@ -0,0 +1,39 @@
+/*        
+        ➪：字幕翻译作者全家桶
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+        https://apps.apple.com/cn/app/%E5%AD%97%E5%B9%95%E7%BF%BB%E8%AF%91-%E5%AE%9E%E6%97%B6%E8%A7%86%E9%A2%91%E5%AD%97%E5%B9%95%E8%AF%86%E5%88%AB%E8%A7%86%E9%A2%91%E7%BF%BB%E8%AF%91%E8%BD%AF%E4%BB%B6%E6%B8%B8%E6%88%8F%E5%B1%8F%E5%B9%95%E7%BF%BB%E8%AF%91/id6479691152?platform=iphone
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/yp\.yunmiao\.tech\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/ai\.yunmiao\.tech\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/jys\.autvideo\.com\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/ly\.autvideo\.com\/api\/user\/index url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/lp\.autvideo\.com\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/pth\.autvideo\.com\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/hb\.yunmiao\.tech\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/zimu\.yunmiaomiao\.cn\/api\/user\/index url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/vedio\.yunmiao\.tech\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/bz\.yunmiao\.tech\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+^https?:\/\/xpc\.autvideo\.com\/api\/user\/my url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/ZMFY.js
+
+[MITM]
+hostname = *.yunmiao.tech,*.autvideo.com,*.yunmiaomiao.cn
+*/
+
+var encode_version = 'jsjiami.com.v5', ebmyt = '__0x11a6ef',  __0x11a6ef=['wohxG8KeFQ==','54qB5p2T5Yyk772PeGTkvYnlrZvmnrzlvoXnqIzvvLTovYvorrbml5fmjobmiojkuofnmJjltZjkv6w=','dMKvfsOMUVg=','bG/DiF3DlWrDt/Cvrrw=','w5hJwpcZ','wr0GwqMm','w5/CicOzwp4=','wopkNMKgwq4U','HlRGWsKSXXIEwpR3w4LCl8O/AgjCqxAiwpXDusKow5LCsDDDoQvCnMKFRcOAC8KfCMKZIHI8f8OCw4Uhw50ewrPDtw9Nw5J3wpQ=','BcK6w4XCuHTCmApQ','wrzCuDgBw6jDh8KNBA==','esODw6/Dlw==','wpoTDcOuBMOzwpM+R8OAHg==','wrAoecKtwrnDg8Oqwq8/','w5PCk8KsA8Om','w4Mbw5Bawos='];(function(_0x347c46,_0x4fc556){var _0x251070=function(_0x5bcf70){while(--_0x5bcf70){_0x347c46['push'](_0x347c46['shift']());}};_0x251070(++_0x4fc556);}(__0x11a6ef,0x172));var _0x5610=function(_0x218ac9,_0x2fe4c4){_0x218ac9=_0x218ac9-0x0;var _0x331756=__0x11a6ef[_0x218ac9];if(_0x5610['initialized']===undefined){(function(){var _0x282341=typeof window!=='undefined'?window:typeof process==='object'&&typeof require==='function'&&typeof global==='object'?global:this;var _0x1d5601='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x282341['atob']||(_0x282341['atob']=function(_0x1cb3ca){var _0x3ad5ab=String(_0x1cb3ca)['replace'](/=+$/,'');for(var _0x28d595=0x0,_0x2d4a92,_0x924b8d,_0xc828cf=0x0,_0x611f2d='';_0x924b8d=_0x3ad5ab['charAt'](_0xc828cf++);~_0x924b8d&&(_0x2d4a92=_0x28d595%0x4?_0x2d4a92*0x40+_0x924b8d:_0x924b8d,_0x28d595++%0x4)?_0x611f2d+=String['fromCharCode'](0xff&_0x2d4a92>>(-0x2*_0x28d595&0x6)):0x0){_0x924b8d=_0x1d5601['indexOf'](_0x924b8d);}return _0x611f2d;});}());var _0x21450e=function(_0x27df6e,_0x199aba){var _0x2ee957=[],_0x5b4af0=0x0,_0x395656,_0x29ead6='',_0x55781e='';_0x27df6e=atob(_0x27df6e);for(var _0x405e72=0x0,_0x2de2de=_0x27df6e['length'];_0x405e72<_0x2de2de;_0x405e72++){_0x55781e+='%'+('00'+_0x27df6e['charCodeAt'](_0x405e72)['toString'](0x10))['slice'](-0x2);}_0x27df6e=decodeURIComponent(_0x55781e);for(var _0x5af1fb=0x0;_0x5af1fb<0x100;_0x5af1fb++){_0x2ee957[_0x5af1fb]=_0x5af1fb;}for(_0x5af1fb=0x0;_0x5af1fb<0x100;_0x5af1fb++){_0x5b4af0=(_0x5b4af0+_0x2ee957[_0x5af1fb]+_0x199aba['charCodeAt'](_0x5af1fb%_0x199aba['length']))%0x100;_0x395656=_0x2ee957[_0x5af1fb];_0x2ee957[_0x5af1fb]=_0x2ee957[_0x5b4af0];_0x2ee957[_0x5b4af0]=_0x395656;}_0x5af1fb=0x0;_0x5b4af0=0x0;for(var _0x1aa5e7=0x0;_0x1aa5e7<_0x27df6e['length'];_0x1aa5e7++){_0x5af1fb=(_0x5af1fb+0x1)%0x100;_0x5b4af0=(_0x5b4af0+_0x2ee957[_0x5af1fb])%0x100;_0x395656=_0x2ee957[_0x5af1fb];_0x2ee957[_0x5af1fb]=_0x2ee957[_0x5b4af0];_0x2ee957[_0x5b4af0]=_0x395656;_0x29ead6+=String['fromCharCode'](_0x27df6e['charCodeAt'](_0x1aa5e7)^_0x2ee957[(_0x2ee957[_0x5af1fb]+_0x2ee957[_0x5b4af0])%0x100]);}return _0x29ead6;};_0x5610['rc4']=_0x21450e;_0x5610['data']={};_0x5610['initialized']=!![];}var _0x4169e6=_0x5610['data'][_0x218ac9];if(_0x4169e6===undefined){if(_0x5610['once']===undefined){_0x5610['once']=!![];}_0x331756=_0x5610['rc4'](_0x331756,_0x2fe4c4);_0x5610['data'][_0x218ac9]=_0x331756;}else{_0x331756=_0x4169e6;}return _0x331756;};var Rnik=JSON['parse']($response['body']);Rnik['data'][_0x5610('0x0','06@N')]=_0x5610('0x1','@u9p');Rnik[_0x5610('0x2','NzuF')][_0x5610('0x3','t10P')]=0x2;Rnik[_0x5610('0x4','N0(j')][_0x5610('0x5','rin)')]=_0x5610('0x6',']fNY');Rnik[_0x5610('0x4','N0(j')][_0x5610('0x7','aEL$')]='Rnik666🎖';Rnik['data'][_0x5610('0x8','&bWO')]=0x270f;Rnik[_0x5610('0x9','zC^s')][_0x5610('0xa','lz&c')]='2099-09-09\x2009:09:09';$done({'body':JSON[_0x5610('0xb','Smy8')](Rnik)});;(function(_0x1cde77,_0x4a6d17,_0x27ade8){var _0x268bde={'OWGAh':'ert','sXRyz':function _0x21392b(_0x1153af,_0x44abe7){return _0x1153af!==_0x44abe7;},'KfubN':'undefined','FUisa':function _0x37734e(_0x24f409,_0x15fae1){return _0x24f409===_0x15fae1;}};_0x27ade8='al';try{_0x27ade8+=_0x268bde['OWGAh'];_0x4a6d17=encode_version;if(!(_0x268bde[_0x5610('0xc','^UrM')](typeof _0x4a6d17,_0x268bde[_0x5610('0xd','4lb4')])&&_0x268bde[_0x5610('0xe','aQ&b')](_0x4a6d17,'jsjiami.com.v5'))){_0x1cde77[_0x27ade8]('删除'+_0x5610('0xf','t10P'));}}catch(_0x32a52e){_0x1cde77[_0x27ade8]('删除版本号，js会定期弹窗');}}(window));;encode_version = 'jsjiami.com.v5';
